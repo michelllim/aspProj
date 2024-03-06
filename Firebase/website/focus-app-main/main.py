2cbb8e9f521cf07034e806b9356e002161083d2c
Tristantwj <tristan7an@gmail.com>
2024-03-07 02:17:19 +0800

    dashboard for pressure

--- Firebase/website/focus-app-main/main.py
+++ Firebase/website/focus-app-main/main.py
@@ -19,7 +19,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore, auth
 import pandas as pd
 from joblib import load
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS,cross_origin
 
 
